966ea59e720ae729a4d8f9dedf02879d7759e484
doaa-1996 <doaaobeidat1996@gmail.com>
2021-06-07 16:12:54 +0300

    completed

---
+++ math_series/math_series.py
@@ -0,0 +1,56 @@
+# 0, 1, 1, 2, 3, 5, 8, 13, ...
+# 2, 1, 3, 4, 7, 11, 18, 29, ..
+
+
+def fibonacci(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)       
+
+
+print(fibonacci(0))        
+print(fibonacci(1))        
+print(fibonacci(2))        
+print(fibonacci(3))      
+print(fibonacci(4))      
+
+
+
+def lucas(n):
+    if n==0:
+        return 2
+    if n==1:
+        return 1
+    else:
+        return lucas(n-1)+lucas(n-2)    
+
+print(lucas(0))        
+print(lucas(1))        
+print(lucas(2))        
+print(lucas(3))        
+print(lucas(4))  
+
+
+def sum_series (x,y=0,z=1):
+    if y==0 and z==1:
+        return fibonacci(x)
+    if y==2 and z==1:
+        return lucas(x)
+    else:
+        return  sum_series(x - 1, y, z) + sum_series(x - 2, y, z)   
+
+
+print(sum_series(0))
+print(sum_series(1))
+print(sum_series(2))
+print(sum_series(3))
+
+
+
+print(sum_series(0,2,1))
+print(sum_series(1,2,1))
+print(sum_series(2,2,1))
+print(sum_series(3,2,1))
--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,73 @@
 from math_series import __version__
-
+from math_series.math_series import fibonacci, lucas,sum_series
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+# 0, 1, 1, 2, 3, 5, 8, 13, ...
+# 2, 1, 3, 4, 7, 11, 18, 29, ..
+
+
+def test_0():
+    assert fibonacci(0) ==0
+
+    
+def test_1():
+    assert fibonacci(1) ==1
+
+    
+def test_2():
+    assert fibonacci(2) ==1
+
+
+def test_3():
+    assert fibonacci(3) ==2   
+
+
+def test_00():
+    assert lucas(0) == 2
+
+
+def test_11():
+    assert lucas(1) == 1
+
+
+def test_22():
+    assert lucas(2) == 3
+
+
+def test_33():
+    assert lucas(3) == 4
+
+
+
+def test_sum_series0():
+    assert sum_series(0) == 0
+
+
+
+def test_sum_series1():
+    assert sum_series(1) == 1
+
+
+def test_sum_series2():
+    assert sum_series(2) == 1
+
+
+def test_sum_series3():
+    assert sum_series(3) == 2
+
+
+
+def test_sum_series021():
+    assert sum_series(0,2,1) == 2
+
+
+def test_sum_series121():
+    assert sum_series(1,2,1) == 1
+
+
+def test_sum_series221():
+    assert sum_series(2,2,1) == 3
+
